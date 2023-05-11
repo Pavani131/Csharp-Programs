e3ffa5a992b2a8b8362ce6816d3638ed5c4e3914
Pavani131 <115334212+Pavani131@users.noreply.github.com>
2023-05-11 12:06:43 +0530

    Add files via upload

---
+++ Two strings reverse of eachother/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Two_strings_reverse_of_eachother
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string str1 = "hello";
+            string str2 = "olleh";
+            if(str1.Reverse().SequenceEqual(str2))
+            {
+                Console.WriteLine("The strings are reverse of eachother.");
+
+            }
+            else
+            {
+                Console.WriteLine("The strings are not reverse of eachother.");
+            }
+        }
+    }
+}
