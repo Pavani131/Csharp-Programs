4d31922b6c656baa9e7d85aeac8a63dfb161823d
Pavani131 <115334212+Pavani131@users.noreply.github.com>
2023-05-11 12:05:18 +0530

    Add files via upload

---
+++ Reverse the String with recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Reverse_the_String_with_recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "hello world";
+            string reversed = ReverseString(original);
+            Console.WriteLine(reversed);
+
+        }
+        public static string ReverseString(string str)
+        {
+            if (str.Length > 0)
+                return str[str.Length - 1] + ReverseString(str.Substring(0, str.Length - 1));
+            else
+                return str;
+        }
+
+    }
+    
+}
---
+++ no of digits in an integer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace no_of_digits_in_an_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 123456;
+            int count = 0;
+            while(num != 0)
+            {
+                num /= 10;
+                count++;
+            }
+            Console.WriteLine("no of digits:" + count);
+        }
+    }
+}
