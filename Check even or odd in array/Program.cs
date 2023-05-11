e875ccf3bb7f62ecf16c5c7a2d0d98b9b44344b4
Pavani131 <115334212+Pavani131@users.noreply.github.com>
2023-05-11 11:59:37 +0530

    Add files via upload

---
+++ Check even or odd in array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Check_even_or_odd_in_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 8, 6, 3, 9, 5, 2, 1, 7, 4, 10 };
+            
+            foreach(int num in arr)
+            {
+                if(num%2 ==0)
+                {
+                    Console.WriteLine("Even Number");
+                }
+                else
+                {
+                    Console.WriteLine("Odd Number");
+                }
+            }
+        }
+    }
+}
---
+++ check armstrong number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace check_armstrong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, originalNum, remainder, result = 0, n = 0;
+            Console.WriteLine("enter an integer:");
+            num = int.Parse(Console.ReadLine());
+            originalNum = num;
+            while(originalNum != 0)
+            {
+                originalNum /= 10;
+                ++n;
+            }
+            originalNum = num;
+            while(originalNum !=0)
+            {
+                remainder = originalNum % 10;
+                result += (int)Math.Pow(remainder, n);
+                originalNum /= 10;
+
+            }
+            if (result == num)
+                Console.WriteLine(num + "is an Armstrong Number.");
+            else
+                Console.WriteLine(num + "is not an Armstrong Number.");
+
+        }
+    }
+}
